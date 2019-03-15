fa7f9d2735d06bab05640da1081ca609d9bf66da
Karol Gucwa <karolg88@gmail.com>
2019-03-15 11:08:46 +0100

    Updates

---
+++ precache-manifest.79f49ff556de494dbd0629f95d3c48d9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "fddf24c1b1b2b692942910c62fc10e89",
+    "url": "/recipe-puppy-app/static/media/no_results.fddf24c1.jpg"
+  },
+  {
+    "revision": "aab3798198a49c7c99ea6b652a67174b",
+    "url": "/recipe-puppy-app/static/media/cook_avatar.aab37981.png"
+  },
+  {
+    "revision": "b68bde10345a4425aec5",
+    "url": "/recipe-puppy-app/static/js/runtime~main.0f0f6cbd.js"
+  },
+  {
+    "revision": "9aac266104999d3fb31b",
+    "url": "/recipe-puppy-app/static/js/main.91555f3e.chunk.js"
+  },
+  {
+    "revision": "2f22f478eed80a6b08b0",
+    "url": "/recipe-puppy-app/static/js/2.5401cc23.chunk.js"
+  },
+  {
+    "revision": "9aac266104999d3fb31b",
+    "url": "/recipe-puppy-app/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "1ce03c764bf9d08d2cbfa0e4acf3fff9",
+    "url": "/recipe-puppy-app/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/recipepuppy/precache-manifest.29fe76a9d8962466f5dafe58500e3c8e.js"
+  "/recipe-puppy-app/precache-manifest.79f49ff556de494dbd0629f95d3c48d9.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/recipepuppy/index.html", {
+workbox.routing.registerNavigationRoute("/recipe-puppy-app/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
